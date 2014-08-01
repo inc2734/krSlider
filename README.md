40f6e8c4c3bbf174c082ba421e43a16cd5146d6e
Takashi Kitajima <inc@2inc.org>
2014-08-01 14:37:00 +0900

    Fix bug image position in slider

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# KrSlider Version 1.0.0
+# KrSlider Version 1.0.1
 
 ## information
 
@@ -15,21 +15,8 @@ http://2inc.org
 March 25, 2014
 
 ### Modified
+August 1, 2014
 
 ### License
-GPL2
-
-Copyright 2014 Takashi Kitajima (email : inc@2inc.org)
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License, version 2, as
-published by the Free Software Foundation.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+GPLv2
+http://www.gnu.org/licenses/gpl-2.0.html
--- krslider.js
+++ krslider.js
@@ -1,27 +1,13 @@
 /**
  * Plugin Name: KrSlider
  * Description: シンプルなスライドショーを実装するjQueryプラグイン
- * Version: 1.0.0
+ * Version: 1.0.1
  * Author: Takashi Kitajima
  * Author URI: http://2inc.org
  * Created : March 25, 2014
- * Modified:
- * License: GPL2
- *
- * Copyright 2014 Takashi Kitajima (email : inc@2inc.org)
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License, version 2, as
- * published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * Modified: August 1, 2014
+ * License: GPLv2
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  *
  * @param string   type( fade, slide )       切り替えアニメーション
  * @param numeric  duration                  アニメーション時間（ms）
@@ -365,6 +351,7 @@
 						overwidth = inner.width() - canvas.width();
 						left = -overwidth / 2;
 					}
+					inner.css( 'left', left );
 					inner.children( '*:not(img)').css( {
 						height: height,
 						width : width
